980350f9de04a6c6f8b49eccd615d031f4b7c4ec
nolixul <nadia.kaye@gmail.com>
2023-03-19 15:38:24 +0000

    Add empty card for starship and character pages

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import { Card, Grid, Group, Text } from '@mantine/core'
 
 const Characters = () => {
     return (
-        <div>
-            characters
-        </div>
+		<Grid>
+			<Grid.Col span={4}>
+				<Card shadow='sm' padding='lg' radius='md' withBorder>
+					<Group position='apart' mt='md' mb='xs'>
+						<Text weight={500}>Character Title</Text>
+					</Group>
+					<Text size='sm' color='dimmed'>
+						Character details
+					</Text>
+				</Card>
+			</Grid.Col>
+		</Grid>
     );
 };
 
--- src/components/Starships.js
+++ src/components/Starships.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import { Card, Grid, Group, Text } from '@mantine/core'
 
 const Starships = () => {
     return (
-        <div>
-            starships
-        </div>
+		<Grid>
+			<Grid.Col span={4}>
+				<Card shadow='sm' padding='lg' radius='md' withBorder>
+					<Group position='apart' mt='md' mb='xs'>
+						<Text weight={500}>Starship Title</Text>
+					</Group>
+					<Text size='sm' color='dimmed'>
+						Starship details
+					</Text>
+				</Card>
+			</Grid.Col>
+		</Grid>
     );
 };
 
